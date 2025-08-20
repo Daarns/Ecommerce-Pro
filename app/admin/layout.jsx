9285171614aa2bdc2654fe635111d6a735a500cd
Nandana <115970431+Daarns@users.noreply.github.com>
2025-08-20 18:07:03 +0700

    Update layout.jsx

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -1,18 +1,44 @@
+"use client"
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import Sidebar from "./sidebar";
+import { SidebarProvider, useSidebar } from './contexts/SidebarContext';
 
-export default function AdminLayout({ children }) {
+function AdminLayoutContent({ children }) {
+  const { isOpen, isMobile } = useSidebar();
+
   return (
-    <div className="min-h-screen flex bg-background">
+    <div className="min-h-screen bg-slate-50">
       <Sidebar />
-      <div className="flex-1 flex flex-col">
+      
+      {/* Main Content - Responsive margin berdasarkan sidebar state */}
+      <div 
+        className={`
+          min-h-screen transition-all duration-200 ease-out
+          ${isMobile 
+            ? 'ml-0' 
+            : isOpen 
+              ? 'ml-72' 
+              : 'ml-16'
+          }
+        `}
+      >
         <Header />
-        <main className="flex-1 p-6 md:p-10 bg-background">
+        <main className="w-full h-full">
           {children}
         </main>
         <Footer />
       </div>
     </div>
+  );
+}
+
+export default function AdminLayout({ children }) {
+  return (
+    <SidebarProvider>
+      <AdminLayoutContent>
+        {children}
+      </AdminLayoutContent>
+    </SidebarProvider>
   );
 }
